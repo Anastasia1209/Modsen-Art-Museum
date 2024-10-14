a799689d7b96ec35905c13065737aadbdd7f0847
Anastasia <nastya.golodok@mail.ru>
2024-10-14 22:50:35 +0300

    feat: update PaintCard

--- src/components/PaintCard/index.tsx
+++ src/components/PaintCard/index.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './PaintCard.module.css';
 import { Link } from 'react-router-dom';
-
-interface PaintCardProps {
-	id: number;
-	title: string;
-	author: string;
-	imageUrl: string;
-	publicStatus: string;
-}
+import { PaintCardProps } from '../../types/types';
+import bookmark from '../../assets/icons/emptyFav.svg';
+import filledBookmark from '../../assets/icons/fullFav.svg';
 
 const PaintCard: React.FC<PaintCardProps> = ({
 	id,
 	title,
 	author,
 	imageUrl,
-	publicStatus,
+	status,
 }) => {
+	const [isFavorite, setIsFavorite] = useState(false);
+
+	useEffect(() => {
+		const favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+		setIsFavorite(favorites.some((fav: { id: number }) => fav.id === id));
+	}, [id]);
+
+	const handleFavoriteClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		event.stopPropagation();
+		event.preventDefault();
+
+		let favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+
+		if (isFavorite) {
+			favorites = favorites.filter((fav: { id: number }) => fav.id !== id);
+			setIsFavorite(false);
+		} else {
+			const newFavorite = { id, title, author, imageUrl, status };
+			favorites.push(newFavorite);
+			setIsFavorite(true);
+		}
+
+		localStorage.setItem('favorites', JSON.stringify(favorites));
+	};
+
 	return (
 		<Link to={`/paint/${id}`} className={styles.cardLink}>
 			<div className={styles.card}>
@@ -24,10 +44,14 @@ const PaintCard: React.FC<PaintCardProps> = ({
 				<div className={styles.details}>
 					<h3 className={styles.title}>{title}</h3>
 					<p className={styles.author}>{author}</p>
-					<p className={styles.status}>{publicStatus}</p>
+					<p className={styles.status}>{status}</p>
 				</div>
-				<button className={styles.favoriteButton}>
-					<img src="/logo/Icons.svg" alt="favorites"></img>
+				<button className={styles.favoriteButton} onClick={handleFavoriteClick}>
+					<img
+						src={isFavorite ? filledBookmark : bookmark}
+						alt="favorites"
+						className={styles.imgInRound}
+					/>
 				</button>
 			</div>
 		</Link>
