750257b1feba62d41fc613b4cbc21708bdf4383e
Anastasia <nastya.golodok@mail.ru>
2024-10-14 14:51:26 +0300

    feat: update Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-//import museumLogo from '/public/logo/museum.svg'
-import styles from './Header.module.css'
-
+import { Link, useLocation } from 'react-router-dom';
+import styles from './Header.module.css';
 
 export const Header: React.FC = () => {
+	const location = useLocation();
+
 	return (
 		<header className={styles.header}>
 			<div className={styles['header-item']}>
 				<img src="/logo/museum.svg" alt="Museum of Art" className="logo" />
 			</div>
-			<div className={styles['header-item']}>
+			{/* Кнопка Home */}
+			{location.pathname !== '/' && (
+				<Link to="/" className={styles['header-item']}>
+					<span>Home</span>
+				</Link>
+			)}
+			<Link to="/favorites" className={styles['header-item']}>
 				<img src="/logo/bookmark.svg" alt="bookmark" className="logo" />
-				<span className={styles.title} >Your favorites</span>
-			</div>
+				<span className={styles.title}>Your favorites</span>{' '}
+			</Link>
 		</header>
 	);
 };
