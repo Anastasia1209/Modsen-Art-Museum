c6d2535cb66b91e660d924fe1d52020452379e3e
Anastasia <nastya.golodok@mail.ru>
2024-10-14 14:58:56 +0300

    feat: update PaintCard

--- src/components/PaintCard/index.tsx
+++ src/components/PaintCard/index.tsx
@@ -1,27 +1,37 @@
 import React from 'react';
 import styles from './PaintCard.module.css';
+import { Link } from 'react-router-dom';
 
 interface PaintCardProps {
-  title: string;
-  author: string;
-  imageUrl: string;
-  publicStatus: string;
+	id: number;
+	title: string;
+	author: string;
+	imageUrl: string;
+	publicStatus: string;
 }
 
-const PaintCard: React.FC<PaintCardProps> = ({ title, author, imageUrl, publicStatus }) => {
-  return (
-    <div className={styles.card}>
-      <img src={imageUrl} alt={title} className={styles.image} />
-      <div className={styles.details}>
-        <h3 className={styles.title}>{title}</h3>
-        <p className={styles.author}>{author}</p>
-        <p className={styles.status}>{publicStatus}</p>
-      </div>
-      <button className={styles.favoriteButton} >
-        <img src="/logo/Icons.svg" alt='favorites' ></img>
-      </button>
-    </div>
-  );
+const PaintCard: React.FC<PaintCardProps> = ({
+	id,
+	title,
+	author,
+	imageUrl,
+	publicStatus,
+}) => {
+	return (
+		<Link to={`/paint/${id}`} className={styles.cardLink}>
+			<div className={styles.card}>
+				<img src={imageUrl} alt={title} className={styles.image} />
+				<div className={styles.details}>
+					<h3 className={styles.title}>{title}</h3>
+					<p className={styles.author}>{author}</p>
+					<p className={styles.status}>{publicStatus}</p>
+				</div>
+				<button className={styles.favoriteButton}>
+					<img src="/logo/Icons.svg" alt="favorites"></img>
+				</button>
+			</div>
+		</Link>
+	);
 };
 
 export default PaintCard;
