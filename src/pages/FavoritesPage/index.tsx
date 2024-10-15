0645d88a0c8e7862bf47ae6f67e294599c4d9053
Anastasia <nastya.golodok@mail.ru>
2024-10-15 16:44:11 +0300

    feat: add FavoritesPage

--- src/pages/FavoritesPage/index.tsx
+++ src/pages/FavoritesPage/index.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header } from '../../components/Header/index';
-import {Footer} from '../../components/Footer/index'
-import styles from './MainPage.module.css'
+import { Footer } from '../../components/Footer/index';
+import styles from './FavoritesPage.module.css';
+import bookmark from '../../assets/icons/icon.svg';
+import PaintList from '../../components/PaintList';
+import { Paint } from '../../types/types';
 
 const FavoritesPage: React.FC = () => {
-    return (
-        <div className={styles.FavoritesPage}>
-            <Header />
+	const [favorites, setFavorites] = useState<Paint[]>([]);
 
-           <Footer />
-        </div>
-    );
+	useEffect(() => {
+		const storedFavorites = localStorage.getItem('favorites');
+		if (storedFavorites) {
+			setFavorites(JSON.parse(storedFavorites));
+		}
+	}, []);
+
+	return (
+		<div className={styles.FavoritesPage}>
+			<Header />
+			<div className={styles.content}>
+				<p>Here are your</p>
+				<div className={styles.bookmarkContainer}>
+					<div className={styles.bookmark}>
+						<img src={bookmark} alt="" />
+					</div>
+					<span className={styles.highlight}>Favorites</span>
+				</div>
+				<div className={styles.paintList}>
+					{favorites.length > 0 ? (
+						<PaintList artworks={favorites} />
+					) : (
+						<p className={styles.emptyMessage}>
+							You don&apos;t have any favorites yet.
+						</p>
+					)}
+				</div>
+			</div>
+
+			<Footer />
+		</div>
+	);
 };
 
 export default FavoritesPage;
