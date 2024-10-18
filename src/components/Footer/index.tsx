abd1ba46ebe90c8b3d23bffa82b5ec0f453f1481
Anastasia <nastya.golodok@mail.ru>
2024-10-18 12:05:19 +0300

    feat: update Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import styles from './Footer.module.css'
-
+import styles from './Footer.module.css';
+import museum from '../../assets/logo/museum-black.svg';
+import modsen from '../../assets/logo/modsen.svg';
 
 export const Footer: React.FC = () => {
 	return (
 		<footer className={styles.footer}>
 			<div className={styles['footer-item']}>
-				<img src="/logo/museum-black.svg" alt="Museum of Art" className="logo" />
+				<img src={museum} alt="Museum of Art" className="logo" />
 			</div>
 			<div className={styles['footer-item']}>
-				<img src="/logo/modsen.svg" alt="modsen" className="logo" />
+				<img src={modsen} alt="modsen" className="logo" />
 			</div>
 		</footer>
 	);
