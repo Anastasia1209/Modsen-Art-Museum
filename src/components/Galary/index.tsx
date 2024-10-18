c7acb20dcee460c49238a5dcc7700a549f62ff8e
Anastasia <nastya.golodok@mail.ru>
2024-10-18 20:25:20 +0300

    feat: move pagination into a separate component

--- src/components/Galary/index.tsx
+++ src/components/Galary/index.tsx
@@ -1,40 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
+
 import GalleryCard from '../GalaryCard';
 import styles from './Galary.module.css';
+import { GalleryProps } from '../../types/types';
 
-interface Painting {
-	id: number;
-	title: string;
-	author: string;
-	status: string;
-	imageUrl: string;
-}
-
-interface GalleryProps {
-	paintings: Painting[];
-	totalPages: number;
-}
-
-const Gallery: React.FC<GalleryProps> = ({ paintings, totalPages }) => {
-	const [currentPage, setCurrentPage] = useState(1);
-	const itemsPerPage = 2;
-	//const totalPages = Math.ceil(localPaintings.length / itemsPerPage);
-
-	//const [visiblePages, setVisiblePages] = useState([1, 2, 3, 4]);
-	const visiblePaintings = paintings.slice(
-		(currentPage - 1) * itemsPerPage,
-		currentPage * itemsPerPage
-	);
-	const handlePageChange = (page: number) => {
-		setCurrentPage(page);
-	};
-
+const Gallery: React.FC<GalleryProps> = ({ paintings }) => {
 	return (
 		<div className={styles.gallery}>
 			<p className={styles.highlight}>Topics for you</p>
 			<p className={styles.title}>Our special gallery</p>
 			<div className={styles.cards}>
-				{visiblePaintings.map((painting) => (
+				{paintings.map((painting) => (
 					<GalleryCard
 						key={painting.id}
 						title={painting.title}
@@ -44,25 +20,6 @@ const Gallery: React.FC<GalleryProps> = ({ paintings, totalPages }) => {
 						id={painting.id}
 					/>
 				))}
-			</div>
-
-			<div className={styles.pagination}>
-				{Array.from({ length: totalPages }, (_, index) => (
-					<button
-						key={index + 1}
-						className={`${styles.pageButton} ${currentPage === index + 1 ? styles.active : ''}`}
-						onClick={() => handlePageChange(index + 1)}
-					>
-						{index + 1}
-					</button>
-				))}
-				<button
-					className={`${styles.nextButton}`}
-					onClick={() => handlePageChange(currentPage + 1)}
-					disabled={currentPage === totalPages}
-				>
-					{'>'}
-				</button>
 			</div>
 		</div>
 	);
