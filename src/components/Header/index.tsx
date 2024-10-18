5d0f25dd0a599536278370825ca664f6a4e15254
Anastasia <nastya.golodok@mail.ru>
2024-10-18 12:04:30 +0300

    feat: update Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import styles from './Header.module.css';
+import museum from '../../assets/logo/museum.svg';
+import home from '../../assets/icons/home.svg';
+import bookmark from '../../assets/icons/bookmark.svg';
+import burger from '../../assets/icons/burger.svg';
 
 export const Header: React.FC = () => {
 	const location = useLocation();
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen);
+	};
 
 	return (
 		<header className={styles.header}>
 			<div className={styles['header-item']}>
-				<img src="/logo/museum.svg" alt="Museum of Art" className="logo" />
+				<img src={museum} alt="Museum of Art" className="logo" />
 			</div>
-			{/* Кнопка Home */}
-			{location.pathname !== '/' && (
-				<Link to="/" className={styles['header-item']}>
-					<span>Home</span>
+
+			<div className={styles.burgerMenu} onClick={toggleMenu}>
+				<img src={isMenuOpen ? burger : burger} alt="Menu" />
+			</div>
+
+			<div className={`${styles.headerItems} ${isMenuOpen ? styles.open : ''}`}>
+				{location.pathname !== '/' && (
+					<Link to="/" className={styles['header-item']}>
+						<img src={home} alt="home" className="logo" />
+						<span className={styles.title}>Home</span>
+					</Link>
+				)}
+				<Link to="/favorites" className={styles['header-item']}>
+					<img src={bookmark} alt="bookmark" className="logo" />
+					<span className={styles.title}>Your favorites</span>{' '}
 				</Link>
-			)}
-			<Link to="/favorites" className={styles['header-item']}>
-				<img src="/logo/bookmark.svg" alt="bookmark" className="logo" />
-				<span className={styles.title}>Your favorites</span>{' '}
-			</Link>
+			</div>
 		</header>
 	);
 };
