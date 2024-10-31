8494e591a1de69533dfdd2417d6c58e71edffe8f
Anastasia <nastya.golodok@mail.ru>
2024-10-31 17:50:39 +0300

    feat: added mapping for routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,16 +1,11 @@
-import React from 'react';
+import { routesConfig } from 'constants/routes';
 import { Route, Routes } from 'react-router-dom';
-import DetailsPage from './pages/DetailsPage';
-import MainPage from './pages/MainPage/index';
-import FavoritesPage from './pages/FavoritesPage';
-import Error404 from './pages/Error404';
 
 const RoutesComponent = () => (
 	<Routes>
-		<Route path="/" element={<MainPage />} />
-		<Route path="/paint/:id" element={<DetailsPage />} />
-		<Route path="/favorites" element={<FavoritesPage />} />
-		<Route path="*" element={<Error404 />} />
+		{routesConfig.map(({ path, component: Component }) => (
+			<Route key={path} path={path} element={<Component />} />
+		))}
 	</Routes>
 );
 
