b11f5b0426c4342522a919b4ec9cfee2d5858c85
Anastasia <nastya.golodok@mail.ru>
2024-10-31 17:57:23 +0300

    feat: updated validationSchema

--- src/validation/validationSchema.ts
+++ src/validation/validationSchema.ts
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 export const searchValidationSchema = yup.object().shape({
   query: yup
     .string()
-    .required('Запрос не может быть пустым')
-    .min(3, 'Запрос должен содержать минимум 3 символа')
-    .matches(/^[a-zA-Zа-яА-Я0-9\s]+$/, 'Запрос содержит недопустимые символы'),
+    .required('Request cannot be empty')
+    .min(3, 'Request must contain at least 3 characters')
+    .matches(/^[a-zA-Zа-яА-Я0-9\s]+$/, 'Request contains invalid characters'),
 });
