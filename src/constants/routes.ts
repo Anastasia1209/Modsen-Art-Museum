69611e314a26fb42736595e4160a0acb22fe8bd7
Anastasia <nastya.golodok@mail.ru>
2024-10-31 17:48:37 +0300

    feat: update constants

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,6 +1,18 @@
+import DetailsPage from '../pages/DetailsPage';
+import Error404 from '../pages/Error404';
+import FavoritesPage from '../pages/FavoritesPage';
+import MainPage from '../pages/MainPage/index';
+
 export const ROUTES = {
     HOME: '/',
     FAVORITES: '/favorites',
     DETAILS: '/paint/:id',
     ERROR: '*',
 }
+
+export const routesConfig = [
+	{ path: ROUTES.HOME, component: MainPage },
+	{ path: ROUTES.DETAILS, component: DetailsPage },
+	{ path: ROUTES.FAVORITES, component: FavoritesPage },
+	{ path: ROUTES.ERROR, component: Error404 },
+];
